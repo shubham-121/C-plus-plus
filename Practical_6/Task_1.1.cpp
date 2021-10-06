81d0a1f1b006fb0bff6a72d5bd97f86d57211ce2
Shivamdobhal <73071952+Shivamdobhal@users.noreply.github.com>
2021-10-06 22:13:16 +0530

    Create Task_1.1.cpp

---
+++ Practical_6/Task_1.1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int x=10,c=2;
+float d=5.55,f=8.8;
+string s="shivam";
+cout<<"\nsum of int and float="<<x+d;
+cout<<"\nsum of two integer = "<<x+c;
+cout<<"\nsum of two float="<<d+f;
+//cout<<"sum of two integer and string ="<<x+s;
+
+return 0;
+}
