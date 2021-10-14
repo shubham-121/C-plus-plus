22ead6f6cd3a29b6944a06025ad437d2d28ed136
Shivamdobhal <73071952+Shivamdobhal@users.noreply.github.com>
2021-10-14 10:48:52 +0530

    Update Task_3.4_code.cpp

--- Practical_7/Task_3.4_code.cpp
+++ Practical_7/Task_3.4_code.cpp
@@ -1,1 +1,30 @@
-hj
+#include <iostream>
+#define size 3
+using namespace std;
+int main()
+{
+    int arr[size][size]={1,2,3,4,5,6,7,8,9};
+    int i=0,j=0;
+    int b[size][size];
+cout<<"Input array is :"<<"\n";
+for(i=0;i<size;i++)
+{
+    
+for(j=0;j<size;j++)
+        cout<<" "<<arr[i][j] ;
+cout<<"\n";
+}
+cout<<"output elements of new array is :"<<"\n";                         
+for(i=0;i<size;i++)
+{
+    for(j=0;j<size;j++)
+        {   //cout<<" "<<arr[i][j] ;
+           b[i][j]=10 - arr[i][j];                                           //RESTORING TO NEW ARRAY BY USING FORMULA ,i.e,10-arr[i][j]
+           cout<<" "<<b[i][j];
+        }    
+cout<<"\n";
+
+}
+return 0;
+}
+
