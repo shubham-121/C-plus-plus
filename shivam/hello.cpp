143e6010560f19c93f8f67ef012ceac1658d1977
shubham-121 <81768689+shubham-121@users.noreply.github.com>
2021-11-25 21:03:28 +0530

    Update hello.cpp

--- shivam/hello.cpp
+++ shivam/hello.cpp
@@ -3,7 +3,7 @@ using namespace std;
 #define max 200;
 int main()
 {
-cout<<"hello Akash sir I am Shivam Dobhal from section C";
+cout<<"hello Akash sir I am Shubham bhatt from section C";
 cout<<"max value "<<max;
 return 0;
 } 
